c1b583170ae1bccf984c25c984605a21968c3621
mvn-namnguyen6-dn <nam.nguyen6@monstar-lab.com>
2022-06-24 15:45:24 +0700

    upsource

---
+++ controllers/base_controller.php
@@ -0,0 +1,26 @@
+
+# controllers/base_controller.php
+
+<?php
+class BaseController
+{
+  protected $folder; 
+
+ 
+  function render($file, $data = array())
+  {
+  
+    $view_file = 'views/' . $this->folder . '/' . $file . '.php';
+    if (is_file($view_file)) {
+      
+     
+      require_once($view_file);
+      $content = ob_get_clean();
+      
+      require_once('views/layouts/application.php');
+    } else {
+      
+      header('Location: index.php?controller=pages&action=error');
+    }
+  }
+}
---
+++ views/posts/show.php
@@ -0,0 +1,7 @@
+# views/posts/show.php
+
+<?php
+  echo "Tiêu đề: $post->title";
+  echo "\n";
+  echo "Nội dung: $post->content";
+?>
